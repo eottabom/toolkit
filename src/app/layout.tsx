987db28725b300a12be548b10ddb63473170b7e1
eottabom <snoopy12oyk@gmail.com>
2026-02-11 20:32:46 +0900

    chore: add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Sora } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const sora = Sora({
@@ -26,6 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5103032140213770"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${sora.variable} ${plexMono.variable} antialiased`}
       >
