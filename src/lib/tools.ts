e0699388737ddea2807e903bfaa03ce41f8dffe5
eottabom <snoopy12oyk@gmail.com>
2026-02-14 09:11:17 +0900

    feat: jsonviewer 기능 추가
    - Raw Json 입력/정리 가능
    - 오른쪽 트리 뷰에서 구조 확인 수정
    - 전체 복사 기능

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -8,6 +8,13 @@ export type ToolItem = {
 
 export const tools: ToolItem[] = [
   {
+    title: "Json Viewer",
+    desc: "Pretty or Edit Json.",
+    tag: "Utility",
+    slug: "json-viewer",
+    createdAt: "2026-02-14",
+  },
+  {
     title: "Base64 Encode/Decode",
     desc: "Encode plain text to Base64 or decode Base64 back to text.",
     tag: "Utility",
--- src/tools/index.ts
+++ src/tools/index.ts
@@ -2,12 +2,14 @@ import type { ComponentType } from "react";
 import type { ToolItem } from "@/lib/tools";
 import DiffPage from "./diff";
 import Base64Tool from "./base64";
+import JsonViewerTool from "@/tools/jsonviewer";
 
 type ToolComponentProps = {
   tool: ToolItem;
 };
 
 export const toolPages: Record<string, ComponentType<ToolComponentProps>> = {
+  "json-viewer": JsonViewerTool,
   "base64": Base64Tool,
   "diff": DiffPage
 };
