0d7d2a7bcf9dc1c26d334118d7bb4d5af0e32960
eottabom <snoopy12oyk@gmail.com>
2026-02-18 16:02:21 +0900

    test

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
   trailingSlash: true,
   basePath,
   assetPrefix: basePath || undefined,
